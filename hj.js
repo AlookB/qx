400d42ef3ba73b731c6de92aee5b4f2604c51fa8
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 16:41:52 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -3,7 +3,7 @@
 
 [rewrite_local]
 #读不舍手
-^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
+^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
 
 #西窗烛 VIP
 ^https?:\/\/lchttpapi\.xczim\.com url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/xcz.js
