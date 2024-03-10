5183287734061ed30fcd6ba06d170de263dcad18
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 16:26:01 +0800

    Update dbss.js

--- dbss.js
+++ dbss.js
@@ -2,8 +2,7 @@
 App: PureLibro
 
 [rewrite_local]
-^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/PureLibro.js
-
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
 [mitm] 
 hostname = api.revenuecat.com
 
