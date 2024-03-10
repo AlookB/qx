75e31cee9ad7f56dd4336057a9af6d358ddabe82
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 16:42:12 +0800

    Update dbss.js

--- dbss.js
+++ dbss.js
@@ -2,7 +2,7 @@
 App: 读不舍手
 
 [rewrite_local]
-^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
+^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
 [mitm] 
 hostname = api.revenuecat.com
 
