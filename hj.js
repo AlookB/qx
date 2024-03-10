b39c5d06c342676978f8364f42130856014c2aec
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 16:29:37 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -6,13 +6,12 @@
 ^https?:\/\/lchttpapi\.xczim\.com url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/xcz.js
 
 #小习惯 VIP
-^https?:\/\/xianbeikeji\.com\/daily\/app\/user\/query$
+^https?:\/\/xianbeikeji\.com\/daily\/app\/user\/query$ url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/xxg.js
 
 #PureLibro VIP
-^https://api.revenuecat.com/v1/(receipts|subscribers)/*
-
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/PureLibro.js
 #读不舍手
-
+^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
 
 [MITM]
 hostname = api.revenuecat.com,lchttpapi.xczim.com,xianbeikeji.com,
