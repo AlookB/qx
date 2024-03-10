fe42884e474bc8e9f4d2fb0a8e218cbde24ce194
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 18:06:58 +0800

    更新 dbss.js

--- dbss.js
+++ dbss.js
@@ -13,8 +13,8 @@ var objc = JSON.parse($response.body);
     objc = 
 
 {
-  "request_date" : "2024-03-10T07:19:46Z",
-  "request_date_ms" : 1710055186394,
+  "request_date_ms" : 1710065048540,
+  "request_date" : "2024-03-10T10:04:08Z",
   "subscriber" : {
     "non_subscriptions" : {
 
@@ -24,39 +24,36 @@ var objc = JSON.parse($response.body);
     "other_purchases" : {
 
     },
-    "management_url" : null,
+    "management_url" : "https://apps.apple.com/account/subscriptions",
     "subscriptions" : {
       "com.valo.reader.vip2.year" : {
-        "is_sandbox" : false,
-        "ownership_type" : "PURCHASED",
-        "billing_issues_detected_at" : null,
-        "period_type" : "normal",
-        "expires_date" : "2099-12-18T01:04:17Z",
-        "grace_period_expires_date" : null,
+        "original_purchase_date" : "2024-01-07T02:27:16Z",
+        "expires_date" : "2099-09-30T11:04:03Z",
+        "is_sandbox" : true,
+        "refunded_at" : null,
+        "store_transaction_id" : "2000000544649818",
         "unsubscribe_detected_at" : null,
-        "original_purchase_date" : "2022-09-08T01:04:18Z",
-        "purchase_date" : "2022-09-08T01:04:17Z",
-        "store" : "app_store"
+        "grace_period_expires_date" : null,
+        "period_type" : "normal",
+        "purchase_date" : "2024-03-10T10:04:03Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
       }
     },
     "entitlements" : {
-      "vip2" : {
-        "expires_date" : "2099-12-18T01:04:17Z",
-        "purchase_date" : "2022-09-08T01:04:17Z",
-        "product_identifier" : "com.valo.reader.vip2.year",
-        "grace_period_expires_date" : null
-      },
       "com.valo.reader.vip2.year" : {
         "grace_period_expires_date" : null,
-        "purchase_date" : "2024-03-09T17:30:54Z",
+        "purchase_date" : "2024-03-10T10:04:03Z",
         "product_identifier" : "com.valo.reader.vip2.year",
-        "expires_date" : "2024-03-09T18:30:54Z"
+        "expires_date" : "2099-09-30T11:04:03Z"
       }
     },
     "original_purchase_date" : "2024-01-07T02:27:16Z",
     "original_app_user_id" : "$RCAnonymousID:cb1253c7edc045bb80d88cc8b45b37ec",
     "last_seen" : "2024-03-10T01:01:04Z"
-  },
+  }
 }
 
         
