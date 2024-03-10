75f2a6cd9e94ac5887356e895709134fb62b1803
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-10 16:29:57 +0800

    Update hj.js

--- hj.js
+++ hj.js
@@ -10,6 +10,7 @@
 
 #PureLibro VIP
 ^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/PureLibro.js
+
 #读不舍手
 ^https://api.revenuecat.com/v1/(receipts|subscribers)/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/dbss.js
 
