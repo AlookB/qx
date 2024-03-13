22c8c94852266c96233087d54b9804dfa40be212
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-03-13 11:14:16 +0800

    更新 xcz.js

--- xcz.js
+++ xcz.js
@@ -3,7 +3,7 @@
 App: 西窗烛 VIP
 
 [rewrite_local]
-^https?:\/\/lchttpapi\.xczim\.com/* url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/xcz.js
+^https?:\/\/lchttpapi\.xczim\.com url script-response-body https://raw.githubusercontent.com/AlookB/qx/main/xcz.js
 
 [MITM]
 hostname = lchttpapi.xczim.com
@@ -12,5 +12,4 @@ hostname = lchttpapi.xczim.com
 var ania = JSON.parse($response.body);
 ania.membership = true;
 ania.lifetimeMembership = true;
-ania.result = true;
 $done({ body: JSON.stringify(ania) });
