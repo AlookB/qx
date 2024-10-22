92fe3a771b4ed24a2111a54b6c38a988db8946b7
优雅，实在是太优雅了 <125810307+AlookB@users.noreply.github.com>
2024-10-22 11:45:45 +0800

    Update wdrj.js

--- wdrj.js
+++ wdrj.js
@@ -4,7 +4,7 @@
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/itunes/3Dscanner.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/AlookB/qx/refs/heads/main/wdrj.js
 
 [mitm] 
 
